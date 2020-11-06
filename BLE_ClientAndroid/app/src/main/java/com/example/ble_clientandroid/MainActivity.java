9733c01c50f7192aa4973680e232b6c107b7996b
Daniel Stankiewicz <traitorablepl@gmail.com>
2020-11-06 07:27:51 +0100

    Updated design and color sets of android app

--- BLE_ClientAndroid/app/src/main/java/com/example/ble_clientandroid/MainActivity.java
+++ BLE_ClientAndroid/app/src/main/java/com/example/ble_clientandroid/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.ble_clientandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(ContextCompat.getColor(getApplicationContext(),R.color.colorPrimaryText));
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+    }
+
+    public void scanDevices(View view){
+        Toast.makeText(getApplicationContext(), "Scanning for devices...", Toast.LENGTH_SHORT).show();
     }
 }
